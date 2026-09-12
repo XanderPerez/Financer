7c6a90e3aa3be7238b175cb00d067558db974b20
XanderPerez <0833762@MY.SCCCD.EDU>
2026-09-11 21:57:50 -0700

    Creation of PaymentReminder.h. It allows the userto set scheduled days/overdue payments to remind them that they need to be payed soon.

---
+++ Financer/PaymentReminder.h
@@ -0,0 +1,21 @@
+#ifndef PAYMENTREMINDER_H
+#define PAYMENTREMINDER_H
+
+#include <iostream>
+#include <string>
+
+class PaymentReminder {
+
+private:
+
+	int reminderDays;
+	bool showOverDuePayments;
+	bool showDueToday;
+
+public:
+
+
+
+};
+
+#endif
